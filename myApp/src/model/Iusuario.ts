f9f6651baee1953a65eccd7c4fd70547414d10b6
alumno <alumno>
2017-05-27 12:10:18 -0300

    ñlkjasdf

---
+++ myApp/src/model/Iusuario.ts
@@ -0,0 +1,16 @@
+export interface IUsuario{
+
+    id:Number;
+    nombre:String;
+    apellido:String;
+    email:String;
+    password:String;
+    fnacimiento:Date;
+    rol:Number;
+    image:String;
+    sexo:String;
+    direccion:String;
+    latitud:Number;
+    longitud:Number;
+    
+}
